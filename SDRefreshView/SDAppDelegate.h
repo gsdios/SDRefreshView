c1cc26fb27fa7cb71a9882f61a7c45c9b0fe5875
GSD_iOS <gsdios@126.com>
2015-02-22 16:57:22 +0800

    Initial Commit

---
+++ SDRefreshView/SDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SDAppDelegate.h
+//  SDRefreshView
+//
+//  Created by aier on 15-2-22.
+//  Copyright (c) 2015年 GSD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SDRefreshViewTests/SDRefreshViewTests.m
@@ -0,0 +1,34 @@
+//
+//  SDRefreshViewTests.m
+//  SDRefreshViewTests
+//
+//  Created by aier on 15-2-22.
+//  Copyright (c) 2015年 GSD. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SDRefreshViewTests : XCTestCase
+
+@end
+
+@implementation SDRefreshViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
