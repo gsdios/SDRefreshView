b05333250a51b4c470b438c0336c94ff76bcee57
GSD_iOS <gsdios@126.com>
2015-04-28 11:51:34 +0800

    去除tabbar环境下底部多出的距离

--- SDRefreshView/Classes/Lib/SDRefreshFooterView.m
+++ SDRefreshView/Classes/Lib/SDRefreshFooterView.m
@@ -45,7 +45,7 @@
     self.activityIndicatorView.hidden = YES;
     _originalScrollViewContentHeight = self.scrollView.contentSize.height;
     self.scrollViewEdgeInsets = UIEdgeInsetsMake(0, 0, self.sd_height, 0);
-    self.center = CGPointMake(self.scrollView.sd_width * 0.5, self.scrollView.contentSize.height + self.sd_height * 0.5 + self.scrollView.contentInset.bottom);
+    self.center = CGPointMake(self.scrollView.sd_width * 0.5, self.scrollView.contentSize.height + self.sd_height * 0.5); // + self.scrollView.contentInset.bottom
     
     self.hidden = [self shouldHide];
 }
@@ -53,9 +53,9 @@
 - (BOOL)shouldHide
 {
     if (self.isEffectedByNavigationController) {
-        return (self.scrollView.bounds.size.height - SDKNavigationBarHeight + self.scrollView.contentInset.bottom > self.sd_y);
+        return (self.scrollView.bounds.size.height - SDKNavigationBarHeight > self.sd_y); //  + self.scrollView.contentInset.bottom
     }
-    return (self.scrollView.bounds.size.height + self.scrollView.contentInset.bottom > self.sd_y);
+    return (self.scrollView.bounds.size.height> self.sd_y); // + self.scrollView.contentInset.bottom
 }
 
 
@@ -64,17 +64,18 @@
     if (![keyPath isEqualToString:SDRefreshViewObservingkeyPath]) return;
     
     CGFloat y = [change[@"new"] CGPointValue].y;
+    CGFloat criticalY = self.scrollView.contentSize.height - self.scrollView.sd_height + self.sd_height; //  + self.scrollView.contentInset.bottom
     
     // 只有在 y>0 以及 scrollview的高度不为0 时才判断
     if ((y <= 0) || (self.scrollView.bounds.size.height == 0)) return;
 
     // 触发SDRefreshViewStateRefreshing状态
-    if (y < (self.scrollView.contentSize.height - self.scrollView.sd_height + self.sd_height + self.scrollView.contentInset.bottom) && (self.refreshState == SDRefreshViewStateWillRefresh)) {
+    if (y < criticalY && (self.refreshState == SDRefreshViewStateWillRefresh)) {
         [self setRefreshState:SDRefreshViewStateRefreshing];
     }
     
     // 触发SDRefreshViewStateWillRefresh状态
-    if (y > (self.scrollView.contentSize.height - self.scrollView.sd_height + self.sd_height + self.scrollView.contentInset.bottom) && (SDRefreshViewStateNormal == self.refreshState)) {
+    if (y > criticalY && (SDRefreshViewStateNormal == self.refreshState)) {
         if (self.hidden) return;
         [self setRefreshState:SDRefreshViewStateWillRefresh];
     }
